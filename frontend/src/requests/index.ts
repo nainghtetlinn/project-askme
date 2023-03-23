f55954e46d0cd9a92d7eb12ededb8e3319e17e83
naing <naing.work.it@gmail.com>
2023-03-23 20:27:23 +0630

    changed backend url

--- frontend/src/requests/index.ts
+++ frontend/src/requests/index.ts
@@ -1,4 +1,8 @@
 import axios from "axios";
 
-export const userInstance = axios.create({ baseURL: "/api/users" });
-export const questionInstance = axios.create({ baseURL: "/api/questions" });
+export const userInstance = axios.create({
+  baseURL: "https://askme-backend.onrender.com/api/users",
+});
+export const questionInstance = axios.create({
+  baseURL: "https://askme-backend.onrender.com/api/questions",
+});
